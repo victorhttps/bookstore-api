f2c522a7accb2a43b04149af53a3ca34bda3f753
victorpereira <dev.victorhugo@gmail.com>
2022-05-24 20:17:23 -0300

    first commit

---
+++ src/test/java/br/com/bookstore/bookstore/BookstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bookstore.bookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
