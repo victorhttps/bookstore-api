6619be4ab42050eb4059ee2d46ccf059adb52030
victorpereira <dev.victorhugo@gmail.com>
2022-05-25 17:51:40 -0300

    classes de dominio iniciadas

--- src/main/java/br/com/bookstore/bookstore/domain/Categoria.java
+++ src/main/java/br/com/bookstore/bookstore/domain/Categoria.java
@@ -3,17 +3,29 @@ package br.com.bookstore.bookstore.domain;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
 @Data
+@Entity
 @NoArgsConstructor
-public class Categoria {
+public class Categoria implements Serializable {
+    private static final long serialVersion = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID")
     private Integer id;
+
+    @Column(name = "Nome")
     private String nome;
+
+    @Column(name = "Descricao")
     private String descricao;
 
+    @OneToMany(mappedBy = "categoria")
     private List<Livro> livros = new ArrayList<>();
 
     public Categoria(String nome, String descricao) {
--- src/main/java/br/com/bookstore/bookstore/domain/Livro.java
+++ src/main/java/br/com/bookstore/bookstore/domain/Livro.java
@@ -2,14 +2,23 @@ package br.com.bookstore.bookstore.domain;
 
 import lombok.Data;
 
-@Data
-public class Livro {
+import javax.persistence.*;
+import java.io.Serializable;
 
+@Data
+@Entity
+public class Livro implements Serializable {
+    private static final long serialVersion = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String titulo;
     private String autor;
     private String texto;
 
+    @ManyToOne
+    @JoinColumn(name = "categoria_id")
     private Categoria categoria;
 
 }
